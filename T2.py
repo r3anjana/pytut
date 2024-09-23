d383aca5ef836135e1014376d76e7bf59a1eb217
rranjana <r.ranjana@nokia.com>
2024-09-23 10:20:24 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ T2.py
@@ -0,0 +1,96 @@
+#PROGRAM TO FIND MAX OF 3 NOS
+# num1=int(input())
+# num2=int(input())
+# num3=int(input())
+# if (num1 >= num2) and (num1 >= num3):
+#    largest = num1
+# elif (num2 >= num1) and (num2 >= num3):
+#    largest = num2
+# else:
+#    largest = num3
+#
+# print("The largest number is", largest)
+
+#PROGRAM TO FIND BIGGEST OF GIVEN TWO NUMS
+# num1=int(input())
+# num2=int(input())
+# if(num1>=num2):
+#     print(num1)
+# else:
+#     print(num2)
+
+#Write a Program to Check whether the given Number is in between 1 and 10
+# n=int(input("Enter Number:"))
+# if n >= 1 and n <= 10:
+#     print("The number",n,"is in between 1 to 10")
+# else:
+#     print("The number",n,"is not in between 1 to 10")
+
+#program to perform arithmetic operations
+# a=int(input())
+# b=int(input())
+# sum=a+b
+# print(sum)
+# sub=a-b
+# print(sub)
+# mul=a*b
+# print(mul)
+# div=a/b
+# print(div)
+# m=a%b
+# print(m)
+
+#program to check whether a given num is even or odd
+# a=int(input())
+# if(a%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+#program to check a num is +ve,-ve or 0.
+# a=int(input())
+# if(a>0):
+#     print("positive")
+# elif(a<0):
+#     print("negative")
+# else:
+#     print("zero")
+
+#program to calculate grade of a student.
+# mark=int(input())
+# if(mark>=90):
+#     print("A")
+# elif(mark<90 and mark>=80):
+#     print("B")
+# elif(mark<80 and mark>=70):
+#     print("C")
+# elif(mark<70 and mark>=60):
+#     print("D")
+# else:
+#     print("F")
+#program to print numbers from 1 to 10
+# for i in range(1, 11):
+#     print(i)
+
+#program to display multiplication table
+# a=int(input())
+# for i in range(1,11):
+#     print(i*a)
+
+#print contents of a file
+# file = open('sample.txt','w')
+# file.write("Hello, this is a sample file")
+# file = open('sample.txt', 'r')
+# content = file.read()
+# print(content)
+# word_list = content.split()
+# num_words = len(word_list)
+# print("Number of words:", num_words)
+
+
+#
+
+
+
+
+
